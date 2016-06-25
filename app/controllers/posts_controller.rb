9701518b29588439ce6795525d39b316c4018a57
mccheung <mc_cheung@126.com>
2016-06-25 10:38:36 +0800

    done for new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+    def new
+        @group = Group.find( params[:group_id])
+        @post = @group.posts.new
+    end
+
+    def create
+        @group = Group.find( params[:group_id])
+        @post = @group.posts.build( post_params )
+
+        if @post.save
+            redirect_to group_path( @group ), notice: "Add post success!"
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def post_params
+        params.require( :post ).permit( :content )
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-    resources :groups
+    resources :groups do
+        resources :posts
+    end
   # You can have the root of your site routed with "root"
     root 'groups#index'
 
