92a43b01b0cc23390d64704f8004436ac04d99ac
mccheung <mc_cheung@126.com>
2016-07-05 21:02:31 +0800

    add account router

---
+++ app/controllers/account/groups_controller.rb
@@ -0,0 +1,7 @@
+class Account::GroupsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        @groups = current_user.participated_groups.order( "posts_count DESC, id DESC")
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+    
+    namespace :account do
+        resources :groups
+        resources :posts
+    end
 
     resources :groups do
         member do
